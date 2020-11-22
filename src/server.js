ea0d6e5e89eb8a59bc81fd43e9ee127b557a9c80
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2020-11-22 10:36:14 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 
-const mongoose = require('mongoose');
-
 const routes = require('./routes');
 
 const app = express();
-
-mongoose.connect('mongodb+srv://patriarca:patriarca@cluster0.jyc97.mongodb.net/patriarca_tapetes?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 app.use(express.json());
 app.use(routes);
